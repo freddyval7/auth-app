02778fdb43f3ebe1fe66dc2d1812112402617d1a
Freddy Tomada <freddytomada7@gmail.com>
2024-06-11 16:36:47 -0400

    add login and register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: "Password is required.",
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required and must be a valid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long.",
+  }),
+  name: z.string({
+    message: "Name is required.",
+  }),
+});
